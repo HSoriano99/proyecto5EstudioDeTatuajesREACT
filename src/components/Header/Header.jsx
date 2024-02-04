8790951c6f7eecb2d46756eb91bb2abdc59407e9
Héctor <hsoriano9986@gmail.com>
2024-02-04 12:18:11 +0100

    estructuramos vista home, ircorporando un Navbar con Bootstrap para luego poder cambiar de vista, registrarnos, logearnos, etc. Ahora mismo hardcodeado para comprobar accesos con token y rol admin

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,64 @@
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Header.css";
+
+
+export const Header = () => {
+
+    //hardcodeamos nuestro token y decoded token para probar los diferentes enlaces
+    const token = 1;
+    const decoded = {
+        role: "JULIO"
+    };
+
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary" id="navbar">
+      <Container>
+        <Navbar.Brand href="">Héctor´s Tattoo Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="personajes">Personajes</Nav.Link>
+            <NavDropdown title="Mi cuenta" id="basic-nav-dropdown">
+              {!token ? (
+                <>
+                  <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+                  <NavDropdown.Item href="register">
+                    Resgistrarse
+                  </NavDropdown.Item>
+                </>
+              ) : decoded.role === "ADMIN" ? (
+                <>
+                  <NavDropdown.Item href="profile">Perfil</NavDropdown.Item>
+                  <NavDropdown.Item href="admin">Admin</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item>
+                    Log out
+                  </NavDropdown.Item>
+                </>
+              ) : (
+                <>
+                  <NavDropdown.Item href="profile">Perfil</NavDropdown.Item>
+                  <NavDropdown.Item href="">Mis citas</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item>
+                    Log out
+                  </NavDropdown.Item>
+                </>
+              )}
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+
+//function onClick de dropdown Logout
+// onClick={() => logMeOut()}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import "./Home.css";
+
+export const Home = () =>{
+
+
+
+
+
+
+
+
+    return (
+        <>
+        <h1>HOLA, ESTA ES LA VISTA HOME DE MI ESTUDIO DE TATUAJES</h1>
+        
+        
+        
+        </>
+    )
+}
