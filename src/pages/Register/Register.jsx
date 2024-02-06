9f2d674f992b618c531cc51efde5fce0931f6793
Héctor <hsoriano9986@gmail.com>
2024-02-06 23:21:45 +0100

    implementación de registro de clientes, a falta de finalizar el proceso de login tras el registro para traer el nuevo token del cliente registrado

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,14 +1,96 @@
+import { jwtDecode } from "jwt-decode";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { CustomInput } from "../../components/LoginInput/LoginImput";
+import { clientRegister, userLogin } from "../../services/ApiCalls";
+import { userData } from "../userSlice";
+import "./Register.css";
 
+export const Register = () => {
+  
+  const [registerData, setRegisterData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    first_name: "",
+    phone_number: "",
+  });
 
-export const Register = () =>{
+  // instancio redux en modo escritura
+  const dispatch = useDispatch();
 
+  // // instancio redux en modo lectura
+  const userRdxData = useSelector(userData);
 
-    return (
-        <>
-        <h1>HOLA, ESTA ES LA VISTA PARA REGISTRARNOS</h1>
-        
-        
-        
-        </>
-    )
-}
+  const inputHandler = (event) => {
+    setRegisterData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  
+  const buttonHandler = () => {
+    const credentials = {
+        email: registerData.email,
+        password: registerData.password,
+    }
+    clientRegister(registerData);
+
+    //hacemos login con el usuario recien creado.
+    userLogin(credentials)
+      .then((token) => {
+        const decodedToken = jwtDecode(token);
+
+        const data = {
+          token: token,
+          userData: decodedToken,
+        };
+        //crear un slice coon un reducer de los datos de registro, no es necesario
+        //pero seguimos usando el reducer de login para hcer login al usuario recién creado
+        dispatch(login({ credentials: data }));
+      })
+      .catch((err) => console.error("ha ocurrido un error", err));
+  };
+
+  console.table(registerData);
+
+  return (
+    <div className="register">
+      <CustomInput
+        placeholder={"escriba un nickname"}
+        type={"username"}
+        name={"username"}
+        handler={inputHandler}
+      ></CustomInput>
+      <CustomInput
+        placeholder={"introduce tu email"}
+        type={"email"}
+        name={"email"}
+        handler={inputHandler}
+      ></CustomInput>
+      <CustomInput
+        placeholder={"escriba su contraseña"}
+        type={"password"}
+        name={"password"}
+        handler={inputHandler}
+      ></CustomInput>
+      <CustomInput
+        placeholder={"escriba su nombre"}
+        type={"first_name"}
+        name={"first_name"}
+        handler={inputHandler}
+      ></CustomInput>
+      <CustomInput
+        placeholder={"escriba su teléfono"}
+        type={"phone_number"}
+        name={"phone_number"}
+        handler={inputHandler}
+      ></CustomInput>
+
+      <div className="apiCallButton" onClick={buttonHandler}>
+        CREAR PERFIL!
+      </div>
+    </div>
+  );
+};
--- src/services/ApiCalls.js
+++ src/services/ApiCalls.js
@@ -3,7 +3,14 @@ import axios from "axios";
 const API_URL = "http://localhost:3000";
 
 export const userLogin = async (credentials) => {
+    console.log(credentials);
     const res = await axios.post(`${API_URL}/api/auth/login`, credentials);
    
     return res.data.token;
 }
+
+export const clientRegister = async (registerData) => {
+    const res = await axios.post(`${API_URL}/api/auth/registerClient`, registerData);
+    return res;
+
+};
