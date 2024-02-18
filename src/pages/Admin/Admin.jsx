b940504956d0f6fc71ace911436b5ecc28062107
Héctor <hsoriano9986@gmail.com>
2024-02-18 12:04:09 +0100

    vista admin con usuarios paginados sacados por pantalla en forma de cartas responsive con un poco de estilos css, falta implementar los botones de paginación y posteriormente el mismo proceso pero para las citas del estudio, ademas de habilitar la funcionalidad del botón de borrar usuarios/citas

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,8 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getUsersPaginated } from "../../services/ApiCalls";
 import { userData } from "../userSlice";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import "./Admin.css"
 
 
 export const Admin = () =>{
@@ -14,8 +17,11 @@ export const Admin = () =>{
     const token = userRdxData.credentials.token;
     const decoded = userRdxData.credentials?.userData;
 
-    const page = 1;
-    const skip = 3;
+    const [Users, setUsers] = useState([]);
+    const [page, setPage] = useState(1);
+    const [skip, setSkip] = useState(3);
+    const [count, setCount] = useState();
+
 
     useEffect(() => {
         if (decoded?.userRoles !== "admin") {
@@ -24,6 +30,10 @@ export const Admin = () =>{
         } else {
             getUsersPaginated(token, page, skip).then((res)=> {
                 console.log(res)
+                setUsers(res.results);
+                setPage(res.page);
+                setSkip(res.skip);
+                setCount(res.count);
             })
         }
       }, []);
@@ -34,11 +44,42 @@ export const Admin = () =>{
 
 
     return (
-        <>
-        <h1>FUTURA FUNCIONALIDAD, DONDE SI TIENES PERFIL ADMIN, PUEDAS ACCEDER A ESTA VISTA Y REALIZAR CONSULTAS COMO ADMIN A LA BASE DE DATOS</h1>
-        
-        
-        
-        </>
+        <div className="adminPage">
+        <div className="pageTitle">
+            <h1>GESTIÓN DEL ESTUDIO</h1>
+        </div>
+        <div className="usersDiv">
+             {Users.map((user, i)=>{
+                return (
+                    <Card className="usercard" key={i} >
+                        <Card.Body>
+                            <Card.Title>{Users[i]?.username}</Card.Title>
+                            <Card.Text>Usuario:
+                                {Users[i]?.role.role_name === "client" ? (
+                                    " Cliente"
+                                ):null}
+                                {Users[i]?.role.role_name === "artist" ? (
+                                    " Tatuador"
+                                ):null}
+                            </Card.Text>
+                            <Card.Text>Email: {Users[i]?.email}</Card.Text>
+                            {Users[i]?.client !== null ? (
+                                         <Card.Text>Tlf. contacto: {Users[i]?.client.phone_number}</Card.Text>
+                            ): null}
+                            {Users[i]?.artist !== null ? (
+                                         <Card.Text>Tlf. contacto: {Users[i]?.artist.phone_number}</Card.Text>
+                            ): null}
+                   
+                            {decoded?.userRoles === "admin" ? (
+                                <Button variant="dark">BORRAR USUARIO</Button>
+                            ): null }
+                            
+                        </Card.Body>
+                     </Card>
+                    )
+                })}
+        </div>
+    
+    </div>
     )
 }
