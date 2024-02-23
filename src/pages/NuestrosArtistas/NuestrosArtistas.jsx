ead527454a48887c47c63befbb72da68571754ca
Héctor <hsoriano9986@gmail.com>
2024-02-24 00:05:02 +0100

    implementada primeras funcionalidades del boton para pedir citas con un tatuador como cliente, puediendo recoger el valor del turno que quieres, posible cambio del select, falta fecha

--- src/pages/NuestrosArtistas/NuestrosArtistas.jsx
+++ src/pages/NuestrosArtistas/NuestrosArtistas.jsx
@@ -5,6 +5,8 @@ import { getArtists } from "../../services/ApiCalls";
 import { userData } from "../userSlice";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import { CustomInput } from "../../components/LoginInput/LoginImput";
 import "./NuestrosArtistas.css"
 
 
@@ -12,6 +14,8 @@ export const NuestrosArtistas = () =>{
     const navigate = useNavigate();
     const userRdxData = useSelector(userData);
     const [Artists, setArtists] = useState([]);
+    const [PedirCita, setPedirCita] = useState(0);
+
 
     const token = userRdxData.credentials.token;
     const id = userRdxData.credentials.userData?.userId;
@@ -28,6 +32,14 @@ export const NuestrosArtistas = () =>{
         console.log( "Soy mis artistas ", Artists )
     },[Artists]);
 
+    const buttonHandlerNewDate = (id) => {
+        if (PedirCita > 0) {
+            setPedirCita(0);
+        } else if (PedirCita === 0) {
+            setPedirCita(id)
+        }
+      }
+
 
     return (
         <div className="artistPage">
@@ -39,13 +51,36 @@ export const NuestrosArtistas = () =>{
                     return (
                         <Card className="artistcard" key={i} >
                             <Card.Img className="artistimg" variant="top" src={Artists[i]?.profile_image} />
-                            <Card.Body>
+                            <Card.Body className="bodyCard">
+                                <div className="artistData">
                                 <Card.Title>{Artists[i]?.first_name}</Card.Title>
                                 <Card.Text>{Artists[i]?.last_name}</Card.Text>
                                 <Card.Text>Tlf. contacto: {Artists[i]?.phone_number}</Card.Text>
                                 <Card.Text>Estilo: {Artists[i]?.tattoo_style}</Card.Text>
-                                {decoded?.userRoles === "client" ? (
-                                    <Button variant="dark">Pedir Cita</Button>
+                                {decoded?.userRoles === "client" && PedirCita === 0  ? (
+                                    <Button variant="dark" onClick={() => buttonHandlerNewDate(Artists[i]?.id)}  >Pedir Cita</Button>
+                                ): null }
+                                {decoded?.userRoles === "client" && PedirCita !== 0 && PedirCita === Artists[i]?.id ? (
+                                    <Button variant="dark" onClick={() => buttonHandlerNewDate(Artists[i]?.id)}  >Otra vez será</Button>
+                                ): null }
+                                </div>
+                                {PedirCita !== 0 && PedirCita === Artists[i]?.id && decoded?.userRoles === "client"? (
+                                    <div className="newAppoitnment">
+                                        <div className="tituloNewAppoitnment">
+                                            <h5>CUANDO QUIERES VENIR?</h5>
+                                        </div>
+                                        <div className="appointmentForm">
+                                            <Form.Select aria-label="Default select example">
+                                                <option>Por la mañana o por la tarde?</option>
+                                                <option value="morning">Mañana</option>
+                                                <option value="afternoon">Tarde</option>
+                                            </Form.Select>
+                                        </div>
+
+
+                                    </div>
+
+
                                 ): null }
                                 
                             </Card.Body>
